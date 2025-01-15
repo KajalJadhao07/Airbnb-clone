a6150c8bf31ecd59ab376f90b724927ae23edbeb
webdev <kajaljadhao16@gmail.com>
2025-01-15 22:22:20 +0530

    Set the default image for the listings

--- models/listing.js
+++ models/listing.js
@@ -1,7 +1,9 @@
+const { ref } = require("joi");
 const mongoose = require("mongoose");
-const schema = mongoose.Schema;
+const Schema = mongoose.Schema;
+const Review = require("./reviews")
 
-const listingSchema = new schema({
+const listingSchema = new Schema({
     title: {
         type: String,
         required: true,
@@ -9,12 +11,29 @@ const listingSchema = new schema({
     description: String,
     image: {
         type: String,
-        default: "https://unsplash.com/photos/palm-tree-near-seashore-Siuwr3uCir0",
-    },
+        default:
+      "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    set: (v) =>
+      v === ""
+        ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+        : v,
+  },
     price: Number,
     location: String,
-    country: String
+    country: String,
+    reviews: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Review",
+      }
+    ]
 });
+
+listingSchema.post("findOneAndDelete", async(listing)=>{
+  if(listing){
+    await Review.deleteMany({_id: {$in: listing.reviews}})
+  }
+})
 
 const listing = mongoose.model("listing", listingSchema);
 module.exports = listing;
